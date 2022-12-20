05fdf04aac78db57914fe6d93ff8f6c2e14051aa
Lucas-Lima-Carioca <120683641+Lucas-Lima-Carioca@users.noreply.github.com>
2022-12-20 17:35:53 -0400

    Create 2022222004.c

---
+++ 2022222004.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int maior, menor, num, i;
+    for (i=0;i<20;i++){
+            printf("Informe o %d numero: ", i+1);
+            scanf("%d", &num);
+        if (i == 0){
+            maior = num;
+            menor = num;
+        } else if(num > maior)
+                maior = num;
+            else if (num < menor)
+                menor = num;
+    }
+    printf("O menor valor foi: %d\n", menor);
+    printf("O maior valor foi: %d\n", maior);
+    return 0;
+}

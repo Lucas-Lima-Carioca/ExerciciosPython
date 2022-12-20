da2d88433576547f7161eb89099b8bec2f5eb2af
Lucas-Lima-Carioca <120683641+Lucas-Lima-Carioca@users.noreply.github.com>
2022-12-20 17:11:03 -0400

    Update 2022222001.c

--- 2022222001.c
+++ 2022222001.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    int num, total = 0;
+    float num, total = 0;
 
     for(int i=0;i<5;i++){
         printf("Informe o %d numero: ",i+1);
-        scanf("%d",&num);
+        scanf("%f",&num);
         total +=num;
     }
-    printf("A soma total eh: %d\n",total);
+    printf("A soma total eh: %.2f\n",total);
     return 0;
 }
